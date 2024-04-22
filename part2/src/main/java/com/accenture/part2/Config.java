bda10ba3fe48e2db907df504cf8581914051301e
Dimitra <132842228+ldimitra@users.noreply.github.com>
2024-04-22 17:00:39 +0300

    Add changes

--- part2/src/main/java/com/accenture/part2/Config.java
+++ part2/src/main/java/com/accenture/part2/Config.java
@@ -18,37 +18,38 @@ public class Config {
     public CommandLineRunner commandLineRunner(InsuredService insuredService, DoctorService doctorService, TimeslotService timeslotService, ReservationService reservationService) {
         return args -> {
             Doctor doctor1 = new Doctor("amka1", "leo", "pavlou");
-            Insured insured1 = new Insured("133569710", "13356971098", "Nikos", "1223", "Anagnostou", "nikos@gmail");
-            Insured insured2 = new Insured("185411269", "18541126934", "Petros", "1223", "Papadakhs", "petros@gmail");
-            Insured insured3 = new Insured("145286903", "14528690389", "Maria", "1223", "Sofou", "maria@gmail");
-            Insured insured4 = new Insured("123457891", "32434232234", "Marialena", "1223", "Arvaniti", "marialena@gmail");
-            insured1.setDoctor(doctor1);
-            insured2.setDoctor(doctor1);
-            insured3.setDoctor(doctor1);
-            insured4.setDoctor(doctor1);
+            Doctor doctor2 = new Doctor("amka2", "neo", "petrou");
+            Insured insured1 = new Insured("133569710", "amka1", "Nikos", "1223", "Anagnostou", "nikos@gmail");
+            Insured insured2 = new Insured("185411269", "amka2", "Petros", "1223", "Papadakhs", "petros@gmail");
+            Insured insured3 = new Insured("145286903", "amka3", "Maria", "1223", "Sofou", "maria@gmail");
+            Insured insured4 = new Insured("123457891", "amka4", "Marialena", "1223", "Arvaniti", "marialena@gmail");
+
             insuredService.addInsured(insured1);
             insuredService.addInsured(insured2);
             insuredService.addInsured(insured3);
             insuredService.addInsured(insured4);
 
             doctorService.addDoctor(doctor1);
+            doctorService.addDoctor(doctor2);
 
-            timeslotService.addTimeslot(new Timeslot("dc1", "20/4/2023 10:30", 40));
-            timeslotService.addTimeslot(new Timeslot("dc2", "21/4/2023 10:30", 40));
+            timeslotService.addTimeslot(new Timeslot("dc1", "24/4/2023 10:30", 40));
+            timeslotService.addTimeslot(new Timeslot("dc2", "29/4/2023 10:30", 40));
             timeslotService.addTimeslot(new Timeslot("dc3", "22/4/2023 10:30", 40));
             timeslotService.addTimeslot(new Timeslot("dc4", "20/5/2023 10:30", 40));
             Timeslot timeslot1 = new Timeslot("cd1", "20/4/2023 10:30", 40);
-            Timeslot timeslot2 = new Timeslot("cd2", "21/4/2023 10:30", 40);
+            Timeslot timeslot2 = new Timeslot("cd2", "28/4/2024 13:30", 40);
             Timeslot timeslot3 = new Timeslot("cd3", "28/4/2024 10:30", 40);
             Timeslot timeslot4 = new Timeslot("cd4", "2/12/2024 20:30", 40);
             timeslotService.addTimeslot(timeslot1);
             timeslotService.addTimeslot(timeslot2);
             timeslotService.addTimeslot(timeslot3);
+            timeslotService.addTimeslot(timeslot4);
 
-            reservationService.addReservation(insured1, timeslot1);
-            reservationService.addReservation(insured2, timeslot2);
-            reservationService.addReservation(insured3, timeslot3);
-            reservationService.addReservation(insured4, timeslot4);
+
+            insuredService.makeAReservation("amka1", "28/4/2024 13:30", "amka1");
+            insuredService.makeAReservation("amka2", "28/4/2024 10:30", "amka1");
+            insuredService.makeAReservation("amka3", "20/5/2023 10:30", "amka2");
+
         };
     }
 }
--- part2/src/main/java/com/accenture/part2/services/InsuredService.java
+++ part2/src/main/java/com/accenture/part2/services/InsuredService.java
@@ -1,19 +1,21 @@
 package com.accenture.part2.services;
 
-import com.accenture.part2.models.Doctor;
-import com.accenture.part2.models.Insured;
-import com.accenture.part2.models.Reservation;
-import com.accenture.part2.models.Timeslot;
+import com.accenture.part2.models.*;
+import com.google.zxing.WriterException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class InsuredService {
+    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/static/img/QRCode.png";
 
     @Autowired
     TimeslotService timeslotService;
@@ -23,6 +25,7 @@ public class InsuredService {
     ReservationService reservationService;
 
     List<Insured> insureds = new ArrayList<>();
+
 
     public List<Insured> addInsured(Insured insured) {
         insureds.add(insured);
@@ -97,6 +100,41 @@ public class InsuredService {
             throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS, "Insured with with AMKA: " + insuredAmka + " cant change reservation over 2 times");
 
         }
+
+    }
+
+
+    public Vaccination getInfoOfInsured(String insuredAmka) {
+
+        for (Insured insured : insureds) {
+            if (Objects.equals(insured.getAmka(), insuredAmka))
+                if (insured.getVaccinationCoverage() != null) {
+
+                    byte[] image = new byte[0];
+                    try {
+
+                        // Generate and Return Qr Code in Byte Array
+                        image = QRCodeGenerator.getQRCodeImage("/vaccinationcoverage/" + insuredAmka + "/qrcode", 250, 250);
+
+                        // Generate and Save Qr Code Image in static/image folder
+                        QRCodeGenerator.generateQRCodeImage("/vaccinationcoverage/" + insuredAmka + "/qrcode", 250, 250, QR_CODE_IMAGE_PATH);
+
+                    } catch (WriterException | IOException e) {
+                        e.printStackTrace();
+                    }
+                    // Convert Byte Array into Base64 Encode String
+                    String qrcode = Base64.getEncoder().encodeToString(image);
+
+
+                    return insured.getVaccinationCoverage();
+//                        return ((new InsuredDTO(insured.getAmka(), insured.getVaccinationCoverage())) (vacc.getExpirationDate()));
+//                        return s;
+                } else
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This insured with AMKA: " + insuredAmka + ", hasn't vaccinated yet!");
+
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error! This insured with AMKA: " + insuredAmka + ", doesn't exists!");
 
     }
 }
